d079c21af4a874f570daf4db03b2ade4e27257cc
Waqas <muhammad.waqas@wearenova.co.uk>
2019-10-22 12:24:42 +0500

    added-basic-data-layer

--- AspDotNetCoreBlog/Presentation/UBlog/Startup.cs
+++ AspDotNetCoreBlog/Presentation/UBlog/Startup.cs
@@ -2,6 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Collections;
+using System.Collections.Generic;
+using UBlog.Data.DummyData;
+using UBlog.Data.Entities;
+using UBlog.Data.Interfaces;
+using UBlog.Data.Repositories;
 
 namespace UBlog
 {
@@ -10,6 +16,9 @@ namespace UBlog
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            var dummyPosts = DummyDataProvider.GetBlogPosts();
+            services.AddScoped<IBlogPostRepository>(p => new BlogPostRepository(dummyPosts));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
