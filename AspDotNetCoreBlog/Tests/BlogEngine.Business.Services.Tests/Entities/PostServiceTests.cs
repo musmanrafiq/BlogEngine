da3951331c6086d878c738a74aa1cb015591e655
usman_solemn <usman.solemn@gmail.com>
2021-06-13 01:15:49 +0500

    Tests added

---
+++ AspDotNetCoreBlog/Tests/BlogEngine.Business.Services.Tests/Entities/PostServiceTests.cs
@@ -0,0 +1,47 @@
+using AutoMapper;
+using BlogEngine.Business.Services.Entities;
+using BlogEngine.Data.Interfaces;
+using BlogEngine.Data.Model.Entities;
+using BlogEngine.Tests.Helpers;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BlogEngine.Business.Services.Tests.Entities
+{
+    public class PostServiceTests
+    {
+        [Fact]
+        public async System.Threading.Tasks.Task PostServiceTests_GetAll_CanGetAllPostsSuccessfully()
+        {
+            // Arrange
+            int numberOfPosts = 3;
+            Mock<IPostRepository> mockRepo = new Mock<IPostRepository>(MockBehavior.Default);
+            mockRepo.Setup(m => m.GetAsync()).ReturnsAsync(value: GeneratePosts(numberOfPosts));
+            IMapper mapper = AutoMapperHelper.SetupMapper();
+
+            // Act
+            var a = new PostService(mockRepo.Object, mapper);
+            var allPosts = await a.GetAll();
+
+            // Assert
+            Assert.Equal(3, allPosts.Count);
+
+        }
+
+        #region private
+
+        private List<Post> GeneratePosts(int noOfPosts)
+        {
+            var tempPosts = new List<Post>();
+
+            for (int i = 0; i < noOfPosts; i++)
+            {
+                tempPosts.Add(new Post() { Id = i, Title = $"{i}" });
+            }
+            return tempPosts;
+        }
+
+        #endregion
+    }
+}
---
+++ AspDotNetCoreBlog/Tests/BlogEngine.Tests.Helpers/AutoMapperHelper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BlogEngine.Business.Services;
+
+namespace BlogEngine.Tests.Helpers
+{
+    public class AutoMapperHelper
+    {
+        public static IMapper SetupMapper()
+        {
+            var config = ModelMapper.Configure();
+            IMapper mapper = config.CreateMapper();
+            return mapper;
+        }
+    }
+}
